fb93b4dd71f22c9d569a6b9c98b7e897b0e286c9
CodervTebeteyke <119060388+CodervTebeteyke@users.noreply.github.com>
2022-11-25 17:45:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+http=require('http');
+path=require('path');
+mysql=require('mysql2');
+express=require('express');
+
+const app = express();
+
+const PORT =  process.env.PORT ?? 3000;
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'static','Main.html'))
+})
+
+app.get('/setOfQuestions',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'static','setsOfQuestions.html'))
+})
+
+app.get('/setQuestionNumber',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'static','setQuestionNumber.html'))
+})
+
+app.get('/adminMain',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'static','adminMain.html'))
+})
+
+app.get('/adminPanel',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'static','adminPanel.html'))
+})
+
+app.get('/adCheckPanel',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'static','adCheckPanel.html'))
+})
+
+app.listen(PORT,()=>{
+    console.log("Server has been working in port 3000")
+})
