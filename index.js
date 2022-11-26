dcc18891fd195783b88b362d39934d0ebbaed180
UnF1l <u.filipp5.6.03@gmail.com>
2022-11-26 17:27:28 +0300

    Day 2

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ http=require('http');
 path=require('path');
 mysql=require('mysql2');
 express=require('express');
+_=require('lodash');
 
 const app = express();
 
 const PORT =  process.env.PORT ?? 3000;
 app.use(express.static(path.resolve(__dirname, 'static')))
 
+
+
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'static','Main.html'))
+    tmp = req.query
+    if (_.isEmpty(tmp)) res.sendFile(path.resolve(__dirname,'static','Main.html'))
+    else
+    {
+        if ((tmp.name === '123') && (tmp.psw === '123')) {
+            //res.sendFile(path.resolve(__dirname,'static','Main.html'))
+            res.redirect('/?id=123')
+        }
+        else res.sendFile(path.resolve(__dirname,'static','Main.html'))
+    }
 })
+
 
 app.get('/setOfQuestions',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'static','setsOfQuestions.html'))
