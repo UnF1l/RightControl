4dd49ee1048402236a0a82bd377ec4abb1613c3b
UnF1l <u.filipp5.6.03@gmail.com>
2022-11-26 05:16:42 +0300

    First page

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ express=require('express');
 const app = express();
 
 const PORT =  process.env.PORT ?? 3000;
+app.use(express.static(path.resolve(__dirname, 'static')))
 
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'static','Main.html'))
